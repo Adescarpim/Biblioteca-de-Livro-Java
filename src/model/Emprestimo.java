7cf6fff00652add9605322e501f5a687a1d53202
Ademir Jose Wilsek Scarpim <ademirscarpim@hotmail.com>
2025-05-11 21:04:58 -0300

    models adicionados

--- src/model/Emprestimo.java
+++ src/model/Emprestimo.java
@@ -1,5 +1,84 @@
 package model;
 
-public class Emprestimo {
-    
+import java.time.LocalDate;
+
+import interfaces.EmprestimoInterface;
+
+public class Emprestimo implements EmprestimoInterface {
+    private Usuario usuario;
+    private Livro livro;
+    private LocalDate dataEmprestimo;
+    private LocalDate dataPrevistaDevolucao;
+    private LocalDate dataDevolucao;
+
+    public Emprestimo(Usuario usuario, Livro livro, LocalDate dataEmprestimo, LocalDate dataPrevistaDevolucao) {
+        this.usuario = usuario;
+        this.livro = livro;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataPrevistaDevolucao = dataPrevistaDevolucao;
+        this.dataDevolucao = null;
+    }
+
+    public void registrarDevolucao(LocalDate dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public boolean estaEmAtraso() {
+        return dataDevolucao != null && dataDevolucao.isAfter(dataPrevistaDevolucao);
+    }
+
+    public long diasDeAtraso() {
+        if (estaEmAtraso()) {
+            return java.time.temporal.ChronoUnit.DAYS.between(dataPrevistaDevolucao, dataDevolucao);
+        }
+        return 0;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    public LocalDate getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(LocalDate dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public LocalDate getDataPrevistaDevolucao() {
+        return dataPrevistaDevolucao;
+    }
+
+    public void setDataPrevistaDevolucao(LocalDate dataPrevistaDevolucao) {
+        this.dataPrevistaDevolucao = dataPrevistaDevolucao;
+    }
+
+    public LocalDate getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(LocalDate dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuário: " + usuario.getNome() + ", Livro: " + livro.getTitulo() +
+               ", Data Empréstimo: " + dataEmprestimo +
+               ", Devolução Prevista: " + dataPrevistaDevolucao +
+               ", Devolução Efetiva: " + (dataDevolucao != null ? dataDevolucao : "Ainda não devolvido");
+    }
 }
--- src/model/Livro.java
+++ src/model/Livro.java
@@ -1,5 +1,96 @@
 package model;
 
 public class Livro {
+    private int codigo;
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+    private String categoria;
+    private int totalExemplares;
+    private int exemplaresDisponiveis;
+
+    public Livro(int codigo, String titulo, String autor, int anoPublicacao, String categoria, int totalExemplares) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+        this.categoria = categoria;
+        this.totalExemplares = totalExemplares;
+        this.exemplaresDisponiveis = totalExemplares;
+    }
+
+    public void emprestar() {
+        if (exemplaresDisponiveis > 0) {
+            exemplaresDisponiveis--;
+        }
+    }
     
+    public void devolver() {
+        if (exemplaresDisponiveis < totalExemplares) {
+            exemplaresDisponiveis++;
+        }
+    }    
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getTotalExemplares() {
+        return totalExemplares;
+    }
+
+    public void setTotalExemplares(int totalExemplares) {
+        this.totalExemplares = totalExemplares;
+    }
+
+    public int getExemplaresDisponiveis() {
+        return exemplaresDisponiveis;
+    }
+
+    public void setExemplaresDisponiveis(int exemplaresDisponiveis) {
+        this.exemplaresDisponiveis = exemplaresDisponiveis;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro [codigo=" + codigo + ", titulo=" + titulo + ", autor=" + autor + ", anoPublicacao="
+                + anoPublicacao + ", categoria=" + categoria + ", totalExemplares=" + totalExemplares
+                + ", exemplaresDisponiveis=" + exemplaresDisponiveis + "]";
+    }
 }
